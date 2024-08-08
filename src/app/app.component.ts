25b5468f1ac9d971360fb0d71aaf4cf793d712b7
paudalida <pauleendalida@gmail.com>
2024-08-08 15:14:45 +0800

    Hindi na balik sa login pag nag refresh -_-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GM18';
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      this.router.navigate(['/main']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
 }
