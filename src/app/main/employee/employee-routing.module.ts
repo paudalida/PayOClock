0edcb626d1b3fdf55efadfec8efbb93c2793cbf9
euniceprotasio <151044273+euniceprotasio@users.noreply.github.com>
2024-08-24 16:38:49 +0800

    pa fix hehehe

--- src/app/main/employee/employee-routing.module.ts
+++ src/app/main/employee/employee-routing.module.ts
@@ -1,44 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
-  // { path: 'dashboard', component: DashboardComponent}, 
-  // {
-  //   path: 'dashboard', 
-  //   component: DashboardComponent, 
-  //   children: [{
-  //     path: '', 
-  //     loadChildren: ()=>import('./components/dashboard/dashboard.module').then((m)=>m.DashboardModule)
-  //   }]
-  // }, 
-  // { path: 'employee', component: EmployeeComponent}, 
-  
-  // {
-  //   path: 'payroll', 
-  //   component: PayrollComponent, 
-  //   children: [{
-  //     path: '', 
-  //     loadChildren: ()=>import('./components/payroll/payroll.module').then((m)=>m.PayrollModule)
-  //   }]
-  // }, 
-  // {
-  //   path: 'payslip', 
-  //   component: PayslipComponent, 
-  //   children: [{
-  //     path: '', 
-  //     loadChildren: ()=>import('./components/payslip/payslip.module').then((m)=>m.PayslipModule)
-  //   }]
-  // }, 
-  // {
-  //   path: 'report', 
-  //   component: ReportComponent, 
-  //   children: [{
-  //     path: '', 
-  //     loadChildren: ()=>import('./components/report/report.module').then((m)=>m.ReportModule)
-  //   }]
-  // },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/main/employee/employee.module.ts
+++ src/app/main/employee/employee.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { CommonModule } from '@angular/common';
 
 import { CommonImportsModule } from '../../modules/common-imports/common-imports.module';
 import { FormsImportsModule } from '../../modules/forms-imports/forms-imports.module';
@@ -17,7 +18,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    CommonModule
     // AdminRoutingModule,
     // CommonImportsModule,
     // FormsImportsModule,
