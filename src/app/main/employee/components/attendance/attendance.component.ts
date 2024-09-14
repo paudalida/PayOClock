6baf3856da66f27baa4ff11fdfc452d039ef00f7
paudalida <pauleendalida@gmail.com>
2024-09-14 15:22:15 +0800

    Attendance table

--- src/app/main/employee/components/attendance/attendance.component.ts
+++ src/app/main/employee/components/attendance/attendance.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface DaySchedule {
+  timeIn: Date;
+  timeOut: Date;
+}
+
 @Component({
   selector: 'app-attendance',
   templateUrl: './attendance.component.html',
@@ -9,6 +14,22 @@ export class AttendanceComponent {
 
   employeeImage: string = '/assets/images/no image.png'; 
   employeeName: string = 'John Doe';
-  employeePosition: string = 'Software Engineer';
-  timeIn: Date = new Date(); 
+  employeePosition: string = 'Employee';
+  timeIn: string = '8:30 AM'
+
+  attendance: {
+    monday: DaySchedule,
+    tuesday: DaySchedule,
+    wednesday: DaySchedule,
+    thursday: DaySchedule,
+    friday: DaySchedule,
+    saturday: DaySchedule
+  } = {
+    monday: { timeIn: new Date('2024-09-11T08:00:00'), timeOut: new Date('2024-09-11T17:00:00') },
+    tuesday: { timeIn: new Date('2024-09-12T08:00:00'), timeOut: new Date('2024-09-12T17:00:00') },
+    wednesday: { timeIn: new Date('2024-09-13T08:00:00'), timeOut: new Date('2024-09-13T17:00:00') },
+    thursday: { timeIn: new Date('2024-09-14T08:00:00'), timeOut: new Date('2024-09-14T17:00:00') },
+    friday: { timeIn: new Date('2024-09-15T08:00:00'), timeOut: new Date('2024-09-15T17:00:00') },
+    saturday: { timeIn: new Date('2024-09-16T08:00:00'), timeOut: new Date('2024-09-16T12:00:00') }
+  };
 }
