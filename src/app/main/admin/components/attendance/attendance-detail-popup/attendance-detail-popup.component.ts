84c8ed27977f20baf227ef4ccc004ed9db914403
Chaws-Jewn <150521807+Chaws-Jewn@users.noreply.github.com>
2024-09-17 23:33:44 +0800

    attendance na gumagana

--- src/app/main/admin/components/attendance/attendance-detail-popup/attendance-detail-popup.component.ts
+++ src/app/main/admin/components/attendance/attendance-detail-popup/attendance-detail-popup.component.ts
@@ -55,19 +55,6 @@ export class AttendanceDetailPopupComponent {
     this.timeInEntered = false; 
   }
 
-  applyManualTime(): void {
-    this.updateManualTime();
-    this.timeInEntered = true; 
-  }
-
-  applyManualOutTime(): void {
-    this.updateManualOutTime();
-    if (this.timeIn) {
-      const timeInCopy = new Date(this.timeIn); 
-      // this.timeOut = new Date(timeInCopy.setHours(this.manualOutHour!, this.manualOutMinute!, 0, 0));
-    }
-  }
-
   formatTime(time: string) {
     let hour = parseInt(time.slice(0, 2), 10);
     let ampm = 'AM';
@@ -90,38 +77,8 @@ export class AttendanceDetailPopupComponent {
         date: this.data.selectedDate
       };
 
-      this.ds.request('POST', 'admin/attendance/manual/user/' + this.employee.id, data).subscribe({
-        next: (res: any) => {
-          this.data.details.time_in  = res.data.time_in;
-          this.data.details.time_out = res.data.time_out;
-          this.data.details.status   = res.data.status;
-          this.data.details.date     = res.data.date;
-          this.pop.toastWithTimer('success', res.message);
-        },
-        error: (err: any) => {
-          this.pop.swalBasic('error', 'Oops! An error has occured', err.error.message);
-        }
-      });
+      this.submit(data);
     }
-  }
-
-  updateManualOutTime(): void {
-    // if (this.manualOutHour !== null && this.manualOutMinute !== null) {
-    //   const hours = this.manualOutHour;
-    //   const minutes = this.manualOutMinute;
-    //   const period = this.manualOutAmPm;
-
-    //   let hours24 = hours!;
-    //   if (period === 'PM' && hours24 < 12) {
-    //     hours24 += 12;
-    //   } else if (period === 'AM' && hours24 === 12) {
-    //     hours24 = 0;
-    //   }
-
-    //   const now = new Date();
-    //   now.setHours(hours24, minutes, 0, 0);
-    //   this.timeOut = now;
-    // }
   }
 
   get forTimeIn() {
@@ -157,12 +114,28 @@ export class AttendanceDetailPopupComponent {
   // }
 
   applyLeave(): void {
-    console.log('Selected Leave Type:', this.selectedLeaveType);
-    this.dialogRef.close({ paidLeave: true });
+    let data = {
+      leave_type: this.selectedLeaveType,
+      date: this.data.selectedDate
+    };
+
+    this.submit(data)
   }
 
-  closePopup() {
-    this.dialogRef.close(this.data);
+  submit(data: any) {
+    this.ds.request('POST', 'admin/attendance/manual/user/' + this.employee.id, data).subscribe({
+      next: (res: any) => {
+        this.closePopup(res.data)
+        this.pop.toastWithTimer('success', res.message);
+      },
+      error: (err: any) => {
+        this.pop.swalBasic('error', 'Oops! An error has occured', err.error.message);
+      }
+    });
+  }
+
+  closePopup(data?: any) {
+    this.dialogRef.close(data);
     this.router.navigate(['/admin/attendance']);
   }
 }
