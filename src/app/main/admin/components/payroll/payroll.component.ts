444f27d2c31b3a73d37f6c7133565ef6883b1209
Chaws-Jewn <150521807+Chaws-Jewn@users.noreply.github.com>
2024-10-10 20:11:48 +0800

    payrolls tab

--- src/app/main/admin/components/payroll/payroll.component.ts
+++ src/app/main/admin/components/payroll/payroll.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { DataService } from '../../../../services/data/data.service';
+import { AdminService } from '../../../../services/admin/admin.service';
+import { PopupService } from '../../../../services/popup/popup.service';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-payroll',
@@ -9,22 +13,62 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./payroll.component.scss']
 })
 export class PayrollComponent implements OnInit {
+  dataSource: any = null;
   fixedColumns: string[] = ['employee_id', 'name', 'position', 'rate'];
-  scrollableColumns: string[] = [
-    'allowance',
-    'contributions',
-    'other_deductions',
-    'total_deductions',
-    'net_salary',
-    'total_salary'
-  ];
-
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  scrollableColumns: any = [];
+  payrolls: any = null;
+  dateFilter: any = null;
+  filterValue = '';
+  columns: any;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit() {
+  constructor(
+    private paginatorIntl: MatPaginatorIntl,
+    private changeDetectorRef: ChangeDetectorRef, 
+    private ds: DataService,
+    private as: AdminService,
+    private pop: PopupService
+  ) {
+    this.paginator = new MatPaginator(this.paginatorIntl, this.changeDetectorRef);
+  }
+
+  ngOnInit(): void {    
+    this.ds.request('GET', 'admin/payrolls').subscribe({
+      next: (res: any) => {
+        this.columns = res.data.columns;
+        this.dateFilter = Object.keys(res.data.columns);
+        this.payrolls = res.data.payrolls;
+        this.filterValue = this.dateFilter[0];
+
+        this.changeData();
+      },
+      error: (err: any) => {
+        this.pop.swalBasic('error', 'Oops! Cannot fetch payrolls!', this.pop.genericErrorMessage);
+      }
+    });
+  }
+
+  get employees() {
+    return this.as.getEmployees();
+  }
+
+  changeData() {
+    this.scrollableColumns = this.columns[this.filterValue];
+
+    console.log(this.scrollableColumns)
+    this.payrolls[this.filterValue].forEach((element: any) => {
+      let employee = this.employees.find((emp: any) => emp.id === element.user_id);
+
+      if(employee) {
+        element.name = employee.full_name;
+        element.position = employee.position;
+        element.employee_id = employee.employee_id;
+      }
+    });
+
+    this.dataSource = new MatTableDataSource<PeriodicElement>(this.payrolls[this.filterValue]);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
@@ -42,42 +86,3 @@ export interface PeriodicElement {
   net_salary: string;
   total_salary: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    employee_id: '1222',
-    name: 'Eiichiro Oda',
-    position: 'Manager',
-    rate: '1000',
-    allowance: '200',
-    contributions: '150',
-    other_deductions: '50',
-    total_deductions: '200',
-    net_salary: '1200',
-    total_salary: '1400'
-  },
-  {
-    employee_id: '1222',
-    name: 'Eiichiro Oda',
-    position: 'Manager',
-    rate: '1000',
-    allowance: '200',
-    contributions: '150',
-    other_deductions: '50',
-    total_deductions: '200',
-    net_salary: '1200',
-    total_salary: '1400'
-  },
-  {
-    employee_id: '1222',
-    name: 'Eiichiro Oda',
-    position: 'Manager',
-    rate: '1000',
-    allowance: '200',
-    contributions: '150',
-    other_deductions: '50',
-    total_deductions: '200',
-    net_salary: '1200',
-    total_salary: '1400'
-  },
-];
